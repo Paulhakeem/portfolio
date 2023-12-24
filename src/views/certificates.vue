ab608fdb8d938ab1c04dea52fb3fcae9239d5385
Paulhakeem <paulnyamawi18@gmail.com>
2023-12-24 17:19:11 +0300

    making some changes

--- src/views/certificates.vue
+++ src/views/certificates.vue
@@ -1,5 +1,13 @@
 <template>
   <div>
+    <RouterLink to="/portfolio">
+      <button
+        class="mx-4 mt-6 bg-gradient-to-r from-blue-500 via-ogray-500 to-green-500 text-white rounded-md p-2 w-22 text-lg uppercase hover:animate-bounce"
+      >
+        go back
+      </button>
+    </RouterLink>
+
     <div
       class="flex flex-wrap mx-6 space-x-6 lg:flex p-5 scroll-smooth justify-center"
     >
@@ -16,7 +24,7 @@
             target="_blank"
           >
             <button
-              class="bg-gradient-to-r from-blue-500 via-ogray-500 to-green-500 text-white rounded-md p-2 w-80 text-lg mt-3 uppercase hover:animate-bounce"
+              class="mt-6 bg-gradient-to-r from-blue-500 via-ogray-500 to-green-500 text-white rounded-md p-2 w-80 text-lg uppercase hover:animate-bounce"
             >
               Open
             </button>
@@ -32,37 +40,12 @@
           <h1 class="text-blue-500 font-semibold">
             Introduction to Front-End Development
           </h1>
-          <a href="https://www.coursera.org/account/accomplishments/certificate/M4LQS2M73MAC" target="_blank">
+          <a
+            href="https://www.coursera.org/account/accomplishments/certificate/M4LQS2M73MAC"
+            target="_blank"
+          >
             <button
               class="bg-gradient-to-l from-blue-500 via-ogray-500 to-green-500 text-white rounded-md p-2 w-80 text-lg mt-3 uppercase hover:animate-bounce"
-            >
-              Open
-            </button>
-          </a>
-        </div>
-      </div>
-
-      <div class="text-lg p-10 rounded-lg shadow-lg w-96">
-        <div class="text-center">
-          <img src="../assets/movie v1.JPG" alt="" class="w-96" />
-        </div>
-        <div class="pt-3">
-          <h1 class="text-blue-500 font-semibold">Movie app V1</h1>
-          <p class="text-pretty text-gray-400 text-md">
-            Simple movie app build using IMDB API
-          </p>
-          <div class="flex gap-4 my-1">
-            <img src="../assets/vue.svg" alt="vue logo" class="w-8" />
-            <img src="../assets/js.svg" alt="javascript logo" class="w-8" />
-            <img
-              src="../assets/tailwindcss.svg"
-              alt="tailwindcss logo"
-              class="w-8"
-            />
-          </div>
-          <a href="https://movie-nu-nine.vercel.app/" target="_blank">
-            <button
-              class="bg-gradient-to-r from-blue-500 via-ogray-500 to-green-500 text-white rounded-md p-2 w-80 text-lg mt-3 uppercase hover:animate-bounce"
             >
               Open
             </button>
@@ -73,4 +56,6 @@
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { RouterLink } from "vue-router";
+</script>

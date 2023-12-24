7c208985ae12563ee47be91b82e47b33f7f7fad7
Paulhakeem <paulnyamawi18@gmail.com>
2023-12-24 17:01:22 +0300

    changing the styling

--- src/views/certificates.vue
+++ src/views/certificates.vue
@@ -1,27 +1,20 @@
 <template>
-    <div>
-   <div
+  <div>
+    <div
       class="flex flex-wrap mx-6 space-x-6 lg:flex p-5 scroll-smooth justify-center"
     >
-      <div class="text-lg p-10 rounded-lg shadow-lg w-96">
+      <div class="text-lg p-10 w-96">
         <div class="text-center">
-          <img src="../assets/medusa.JPG" alt="" class="w-96" />
+          <img src="../assets/cert/cert.JPG" alt="" class="w-96" />
         </div>
         <div class="pt-3">
-          <h1 class="text-blue-500 font-semibold">Online Store</h1>
-          <p class="text-pretty text-gray-400 text-md">
-            Medusa is an online store
-          </p>
-          <div class="flex gap-4 my-4">
-            <img src="../assets/vue.svg" alt="nuxt logo" class="w-10" />
-            <img src="../assets/js.svg" alt="javascript logo" class="w-8" />
-            <img
-              src="../assets/tailwindcss.svg"
-              alt="tailwindcss logo"
-              class="w-8"
-            />
-          </div>
-          <a href="https://medusa-sooty-nine.vercel.app/" target="_blank">
+          <h1 class="text-blue-500 font-semibold">
+            Programming with JavaScript
+          </h1>
+          <a
+            href="https://www.coursera.org/account/accomplishments/certificate/BSDX46S4GW5E"
+            target="_blank"
+          >
             <button
               class="bg-gradient-to-r from-blue-500 via-ogray-500 to-green-500 text-white rounded-md p-2 w-80 text-lg mt-3 uppercase hover:animate-bounce"
             >
@@ -31,25 +24,15 @@
         </div>
       </div>
 
-      <div class="text-lg p-10 rounded-lg shadow-lg w-96">
+      <div class="text-lg p-10 w-96">
         <div class="text-center">
-          <img src="../assets/movie v2.JPG" alt="" class="w-96" />
+          <img src="../assets/cert/coursera.JPG" alt="" class="w-96" />
         </div>
         <div class="pt-3">
-          <h1 class="text-blue-500 font-semibold">Movie App V2</h1>
-          <p class="text-pretty text-gray-400 text-md">
-            Simple movie app build using TMDB API
-          </p>
-          <div class="flex gap-4 my-1">
-            <img src="../assets/vue.svg" alt="nuxt logo" class="w-10" />
-            <img
-              src="../assets/tailwindcss.svg"
-              alt="tailwindcss logo"
-              class="w-8"
-            />
-            <img src="../assets/js.svg" alt="javascript logo" class="w-8" />
-          </div>
-          <a href="https://pre-movies.vercel.app/" target="_blank">
+          <h1 class="text-blue-500 font-semibold">
+            Introduction to Front-End Development
+          </h1>
+          <a href="https://www.coursera.org/account/accomplishments/certificate/M4LQS2M73MAC" target="_blank">
             <button
               class="bg-gradient-to-l from-blue-500 via-ogray-500 to-green-500 text-white rounded-md p-2 w-80 text-lg mt-3 uppercase hover:animate-bounce"
             >
@@ -87,9 +70,7 @@
         </div>
       </div>
     </div>
-    </div>
+  </div>
 </template>
 
-<script setup>
-
-</script>
+<script setup></script>

0e7d47f70f109cbd72018a19607e4cbf779a5a9a
Paulhakeem <paulnyamawi18@gmail.com>
2023-12-25 21:56:39 +0300

    finalizing it

--- src/views/projects.vue
+++ src/views/projects.vue
@@ -4,14 +4,87 @@
     <div
       class="project flex flex-wrap mx-6 space-x-6 lg:flex p-5 scroll-smooth justify-center"
     >
-    <div v-for="project in projects" :key="project.id">
-        <div id="box" class="text-lg p-10 rounded-lg shadow-lg w-96">
+      <div id="box" class="text-lg p-10 rounded-lg shadow-lg w-96">
         <div class="text-center">
-          <img :src="project.image" alt="" class="w-96" />
+          <img src="../assets/medusa.png" alt="" class="w-96" />
         </div>
         <div class="pt-3">
-          <h1 class="text-blue-500 font-semibold">{{project.heading}}</h1>
-          <p class="text-pretty text-gray-400 text-md">{{project.text}}</p>
+          <h1 class="text-blue-500 font-semibold">Online Store</h1>
+          <p class="text-pretty text-gray-400 text-md">
+            Medusa is an online store
+          </p>
+          <div class="img flex gap-4 my-8">
+            <img src="../assets/vue.svg" alt="vue logo" class="w-8" />
+            <img src="../assets/js.svg" alt="javascript logo" class="w-8" />
+            <img
+              src="../assets/tailwindcss.svg"
+              alt="tailwindcss logo"
+              class="w-8"
+            />
+             <img
+              src="../assets/firebase.svg"
+              alt="firebase logo"
+              class="w-8"
+            />
+          </div>
+          <a
+            href="https://medusa-sooty-nine.vercel.app/"
+            target="_blank"
+          >
+            <button
+              class="bg-gradient-to-r from-blue-500 via-ogray-500 to-green-500 text-white rounded-md p-2 w-80 text-lg mt-3 uppercase hover:animate-bounce"
+            >
+              Open
+            </button>
+          </a>
+        </div>
+      </div>
+
+      <div id="box" class="text-lg p-10 rounded-lg shadow-lg w-96">
+        <div class="text-center">
+          <img src="../assets/todo.png" alt="" class="w-96" />
+        </div>
+        <div class="pt-3">
+          <h1 class="text-blue-500 font-semibold">RealTime Todo</h1>
+          <p class="text-pretty text-gray-400 text-md">
+            Simple Realtime todo app
+          </p>
+          <div class="img flex gap-4 my-8">
+            <img src="../assets/vue.svg" alt="vue logo" class="w-8" />
+            <img src="../assets/js.svg" alt="javascript logo" class="w-8" />
+            <img
+              src="../assets/tailwindcss.svg"
+              alt="tailwindcss logo"
+              class="w-8"
+            />
+            <img
+              src="../assets/firebase.svg"
+              alt="firebase logo"
+              class="w-8"
+            />
+          </div>
+          <a
+            href="https://realtime-todo-inky.vercel.app/"
+            target="_blank"
+          >
+            <button
+              class="bg-gradient-to-r from-blue-500 via-ogray-500 to-green-500 text-white rounded-md p-2 w-80 text-lg mt-3 uppercase hover:animate-bounce"
+            >
+              Open
+            </button>
+          </a>
+        </div>
+      </div>
+
+      <div id="box" class="text-lg p-10 rounded-lg shadow-lg w-96">
+        <div class="text-center">
+          <img src="../assets/website.png" alt="" class="w-96" />
+        </div>
+        <div class="pt-3">
+          <h1 class="text-blue-500 font-semibold">Website</h1>
+          <p class="text-pretty text-gray-400 text-md">
+            Simple website
+          </p>
           <div class="img flex gap-4 my-8">
             <img src="../assets/vue.svg" alt="vue logo" class="w-8" />
             <img src="../assets/js.svg" alt="javascript logo" class="w-8" />
@@ -22,7 +95,101 @@
             />
           </div>
           <a
-            href="http://localhost:5173/src/assets/weather%20v1.JPG"
+            href="https://code-with-paul.vercel.app/"
+            target="_blank"
+          >
+            <button
+              class="bg-gradient-to-r from-blue-500 via-ogray-500 to-green-500 text-white rounded-md p-2 w-80 text-lg mt-3 uppercase hover:animate-bounce"
+            >
+              Open
+            </button>
+          </a>
+        </div>
+      </div>
+
+      <div id="box" class="text-lg p-10 rounded-lg shadow-lg w-96">
+        <div class="text-center">
+          <img src="../assets/moviev2.png" alt="" class="w-96" />
+        </div>
+        <div class="pt-3">
+          <h1 class="text-blue-500 font-semibold">Movie App v2</h1>
+          <p class="text-pretty text-gray-400 text-md">
+            Simple movie app build using TMDB API
+          </p>
+          <div class="img flex gap-4 my-3.5">
+            <img src="../assets/vue.svg" alt="vue logo" class="w-8" />
+            <img src="../assets/js.svg" alt="javascript logo" class="w-8" />
+            <img
+              src="../assets/tailwindcss.svg"
+              alt="tailwindcss logo"
+              class="w-8"
+            />
+          </div>
+          <a
+            href="https://pre-movies.vercel.app/"
+            target="_blank"
+          >
+            <button
+              class="bg-gradient-to-r from-blue-500 via-ogray-500 to-green-500 text-white rounded-md p-2 w-80 text-lg mt-3 uppercase hover:animate-bounce"
+            >
+              Open
+            </button>
+          </a>
+        </div>
+      </div>
+
+      <div id="box" class="text-lg p-10 rounded-lg shadow-lg w-96">
+        <div class="text-center">
+          <img src="../assets/weatherv1.png" alt="" class="w-96" />
+        </div>
+        <div class="pt-3">
+          <h1 class="text-blue-500 font-semibold">Weather App v1</h1>
+          <p class="text-pretty text-gray-400 text-md text-balance">
+            Local weather app used by searching a city or a country to get the
+            real time weather of the place
+          </p>
+          <div class="img flex gap-4">
+            <img src="../assets/vue.svg" alt="vue logo" class="w-8" />
+            <img src="../assets/js.svg" alt="javascript logo" class="w-8" />
+            <img
+              src="../assets/tailwindcss.svg"
+              alt="tailwindcss logo"
+              class="w-8"
+            />
+          </div>
+          <a
+            href="https://local-weather-app-red.vercel.app/"
+            target="_blank"
+          >
+            <button
+              class="bg-gradient-to-r from-blue-500 via-ogray-500 to-green-500 text-white rounded-md p-2 w-80 text-lg mt-3 uppercase hover:animate-bounce"
+            >
+              Open
+            </button>
+          </a>
+        </div>
+      </div>
+
+      <div id="box" class="text-lg p-10 rounded-lg shadow-lg w-96">
+        <div class="text-center">
+          <img src="../assets/moviev1.png" alt="" class="w-96" />
+        </div>
+        <div class="pt-3">
+          <h1 class="text-blue-500 font-semibold">movie app v1</h1>
+          <p class="text-pretty text-gray-400 text-md">
+            Simple movie app build using IMDB API
+          </p>
+          <div class="img flex gap-4 my-3.5">
+            <img src="../assets/vue.svg" alt="vue logo" class="w-8" />
+            <img src="../assets/js.svg" alt="javascript logo" class="w-8" />
+            <img
+              src="../assets/tailwindcss.svg"
+              alt="tailwindcss logo"
+              class="w-8"
+            />
+          </div>
+          <a
+            href="https://movie-nu-nine.vercel.app/"
             target="_blank"
           >
             <button
@@ -34,58 +201,62 @@
         </div>
       </div>
     </div>
-    
-    </div>
   </div>
 </template>
 
 <script setup>
-import {ref} from "vue"
-import buttons from "../components/buttons.vue";
-import medusa from "../assets/medusa.png";
-import moviev1 from "../assets/moviev1.png";
-import weatherv1 from "../assets/weatherv1.png";
-import moviev2 from "../assets/moviev2.png";
-import website from "../assets/website.png";
-import todo from "../assets/todo.png";
+// import {ref} from "vue"
+// import buttons from "../components/buttons.vue";
+// import medusa from "../assets/medusa.png";
+// import moviev1 from "../assets/moviev1.png";
+// import weatherv1 from "../assets/weatherv1.png";
+// import moviev2 from "";
+// import website from "";
+// import todo from "";
+// import vue from "../assets/vue.svg";
+// import tailwindcss from "../assets/tailwindcss.svg";
+// import nuxt from "../assets/nuxt.png";
+// import js from "../assets/js.svg";
+// import firebase from "../assets/firebase.svg";
 
-let projects = ref([
-  {
-    id: 2,
-    heading: "Online Store",
-    text: "Medusa is an online store",
-    image: medusa,
-  },
-  {
-    id: 2,
-    heading: "Movie app Version I",
-    text: "Simple movie app build using IMDB API",
-    image: moviev1,
-  },
+// let projects = ref([
+//   {
+//     id: 2,
+//     heading: "Online Store",
+//     text: "Medusa is an online store",
+//     languages: [vue, js, firebase],
+//     image: medusa,
+//   },
+//   {
+//     id: 2,
+//     heading: "Movie app Version I",
+//     text: "Simple movie app build using IMDB API",
+//     image: moviev1,
+//   },
 
-  {
-    id: 3,
-    heading: "Movie App Version II",
-    text: " Simple movie app build using TMDB API",
-    image: moviev2,
-  },
-  {
-    id: 4,
-    heading: "RealTime Todo",
-    text: "Simple Realtime todo app",
-    image: todo,
-  },
-  {
-    id: 5,
-    heading: "Weather Version I",
-    text: "Local weather app used by searching a city or a country to get the real time weather of the place",
-    image: weatherv1,
-  },
-  {
-    id: 6,
-    heading: "website",
-    text: "Simple website",
-    image: website,
-  },
-]);
+//   {
+//     id: 3,
+//     heading: "Movie App Version II",
+//     text: " ",
+//     image: moviev2,
+//   },
+//   {
+//     id: 4,
+//     heading: "RealTime Todo",
+//     text: "Simple Realtime todo app",
+//     image: todo,
+//   },
+//   {
+//     id: 5,
+//     heading: "Weather Version I",
+//     text: "",
+//     image: weatherv1,
+//   },
+//   {
+//     id: 6,
+//     heading: "website",
+//     text: "Simple website",
+//     image: website,
+//   },
+// ]);
 </script>

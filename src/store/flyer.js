122df24b72f8dc51824fcf14c49b7dcc2a7e11aa
Paulhakeem <paulnyamawi18@gmail.com>
2023-12-26 17:18:38 +0300

    making it responsive

--- src/store/flyer.js
+++ src/store/flyer.js
@@ -1,11 +1,11 @@
 import { defineStore } from 'pinia'
 import { ref } from "vue";
-import offerflyer from "../assets/graphic/offerflyer.jpg"
-import school from "../assets/graphic/school.jpg"
-import vale from "../assets/graphic/vale.jpg"
-import wasinibirthday from "../assets/graphic/wasinibirthday.jpg"
-import Discover from "../assets/graphic/Discover.jpg"
-import honeymoon from "../assets/graphic/honeymoon.jpg"
+import offerflyer from "../assets/graphic/offerflyer.png"
+import school from "../assets/graphic/school.png"
+import vale from "../assets/graphic/vale.png"
+import wasinibirthday from "../assets/graphic/wasinibirthday.png"
+import Discover from "../assets/graphic/Discover.png"
+import honeymoon from "../assets/graphic/honeymoon.png"
 
 export const useFlyerStore = defineStore('flyers', () => {
    

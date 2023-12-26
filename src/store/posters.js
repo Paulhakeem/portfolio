a2238101ecbf1c4d8bc447e5d734789420c0fc9a
Paulhakeem <paulnyamawi18@gmail.com>
2023-12-26 17:56:40 +0300

    add some styling

--- src/store/posters.js
+++ src/store/posters.js
@@ -1,24 +1,24 @@
 import { defineStore } from 'pinia'
 import { ref } from "vue";
-import Apartment from "../assets/graphic/Apartment.jpg"
+// import Apartment from "../assets/graphic/Apartment.jpg"
 import ADVERT from "../assets/graphic/ADVERT.jpg"
-import bnb from "../assets/graphic/bnb.jpg"
+// import bnb from "../assets/graphic/bnb.jpg"
 import customer from "../assets/graphic/customer.jpg"
 import confirm from "../assets/graphic/confirm.jpg"
 import dolphin from "../assets/graphic/dolphin.jpg"
-import jamhuri from "../assets/graphic/jamhuri.jpg"
-import offer from "../assets/graphic/offer.jpg"
+// import jamhuri from "../assets/graphic/jamhuri.jpg"
+// import offer from "../assets/graphic/offer.jpg"
 import poster11 from "../assets/graphic/poster11.jpg"
 import poster6 from "../assets/graphic/poster6.jpg"
 import FISHION from "../assets/graphic/FISHION.jpg"
 import madaraka from "../assets/graphic/madaraka.jpg"
 import poster4 from "../assets/graphic/poster4.jpg"
 import poster5 from "../assets/graphic/poster5.jpg"
-import wasini from "../assets/graphic/wasini.jpg"
-import newyear from "../assets/graphic/newyear.jpg"
+// import wasini from "../assets/graphic/wasini.jpg"
+// import newyear from "../assets/graphic/newyear.jpg"
 import may from "../assets/graphic/may.jpg"
 import valentines from "../assets/graphic/valentines.jpg"
-import holiday from "../assets/graphic/holiday.jpg"
+// import holiday from "../assets/graphic/holiday.jpg"
 import farmlogo from "../assets/graphic/farmlogo.jpg"
 import waterlogo from "../assets/graphic/waterlogo.jpg"
 import logo from "../assets/graphic/logo.jpg"
@@ -28,10 +28,10 @@ export const usePosterStore = defineStore('posters', () => {
    
     
     let posters = ref([
-      {
-        id: 1,
-        image: Apartment,
-      },
+      // {
+      //   id: 1,
+      //   image: Apartment,
+      // },
       {
         id: 2,
         image: confirm,
@@ -48,18 +48,18 @@ export const usePosterStore = defineStore('posters', () => {
         id: 3,
         image: ADVERT,
       },
-      {
-        id: 3,
-        image: bnb,
-      },
-      {
-        id: 3,
-        image: jamhuri,
-      },
-      {
-        id: 3,
-        image: offer,
-      },
+      // {
+      //   id: 3,
+      //   image: bnb,
+      // },
+      // {
+      //   id: 3,
+      //   image: jamhuri,
+      // },
+      // {
+      //   id: 3,
+      //   image: offer,
+      // },
       {
         id: 3,
         image: poster11,
@@ -84,14 +84,14 @@ export const usePosterStore = defineStore('posters', () => {
         id: 3,
         image: poster5,
       },
-      {
-        id: 3,
-        image: wasini,
-      },
-      {
-        id: 3,
-        image: newyear,
-      },
+      // {
+      //   id: 3,
+      //   image: wasini,
+      // },
+      // {
+      //   id: 3,
+      //   image: newyear,
+      // },
       {
         id: 3,
         image: may,
@@ -100,10 +100,10 @@ export const usePosterStore = defineStore('posters', () => {
         id: 3,
         image: valentines,
       },
-      {
-        id: 3,
-        image: holiday,
-      },
+      // {
+      //   id: 3,
+      //   image: holiday,
+      // },
       {
         id: 3,
         image: waterlogo,

9d7e7f055f0f821027a718db52e2a3559d8a9227
Paulhakeem <paulnyamawi18@gmail.com>
2023-12-26 17:44:10 +0300

    add some styling

--- src/store/flyer.js
+++ src/store/flyer.js
@@ -1,24 +1,24 @@
 import { defineStore } from 'pinia'
 import { ref } from "vue";
-// import offerflyer from "../assets/graphic/offerflyer.jpg"
-// import school from "../assets/graphic/school.png"
-import vale from "../assets/graphic/vale.png"
-import wasinibirthday from "../assets/graphic/wasinibirthday.png"
-import Discover from "../assets/graphic/Discover.png"
-import honeymoon from "../assets/graphic/honeymoon.png"
+import offerflyer from "../assets/graphic/offerflyer.jpg"
+import school from "../assets/graphic/school.jpg"
+import vale from "../assets/graphic/vale.jpg"
+import wasinibirthday from "../assets/graphic/wasinibirthday.jpg"
+import Discover from "../assets/graphic/Discover.jpg"
+import honeymoon from "../assets/graphic/honeymoon.jpg"
 
 export const useFlyerStore = defineStore('flyers', () => {
    
     
     let flyers = ref([
-      // {
-      //   id: 1,
-      //   image: offerflyer,
-      // },
-      // {
-      //   id: 2,
-      //   image: school,
-      // },
+      {
+        id: 1,
+        image: offerflyer,
+      },
+      {
+        id: 2,
+        image: school,
+      },
       {
         id: 3,
         image: vale,
